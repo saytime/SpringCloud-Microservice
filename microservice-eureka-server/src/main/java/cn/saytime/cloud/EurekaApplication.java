49eac24c7efeb70b05f6ae471984fd39d7a094a5
zh <zh@focusmb.com>
2017-07-18 18:09:54 +0800

    服务注册及发现 Eureka

---
+++ microservice-eureka-server/src/main/java/cn/saytime/cloud/EurekaApplication.java
@@ -0,0 +1,21 @@
+package cn.saytime.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author zh
+ * @ClassName cn.saytime.cloud.EurekaApplication
+ * @Description
+ * @date 2017-07-18 16:05:36
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+
+}
